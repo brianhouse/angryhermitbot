3e42ee08b138755c143929e8d2a8cf383cfaf781
brianhouse <brian.house@gmail.com>
2014-06-28 23:10:00 +0200

    comments

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import json
 from housepy import config, log
 from twitter import Twitter, OAuth
 
+# initialize Twitter and retrieve followers
 t = Twitter(auth=OAuth(config['twitter']['access_token'], config['twitter']['access_token_secret'], config['twitter']['consumer_key'], config['twitter']['consumer_secret']))
 try:
     result = t.followers.ids(screen_name="angryhermitbot")
@@ -12,7 +13,7 @@ except Exception as e:
     exit()
 current_ids = result['ids']
 
-
+# sort followers
 past_ids = []
 with open('followers.txt', 'r') as f:
     for line in f.readlines():
@@ -20,15 +21,11 @@ with open('followers.txt', 'r') as f:
 new_ids = list(set(current_ids) - set(past_ids))
 exit_ids = list(set(past_ids) - set(current_ids))
 persisting_ids = list(set(current_ids) - set(new_ids))
-
-
-# debug
-ids = {'new_ids': new_ids, 'exit_ids': exit_ids, 'persisting_ids': persisting_ids}
-print(json.dumps(ids, indent=4))
+log.debug(json.dumps({'new_ids': new_ids, 'exit_ids': exit_ids, 'persisting_ids': persisting_ids}, indent=4))
 
 
 
-
+# save followers
 with open('followers.txt', 'w') as f:
     for id in (new_ids + persisting_ids):
         f.write("%s\n" % id)
