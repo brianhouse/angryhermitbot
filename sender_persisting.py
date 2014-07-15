40bcf7c2af57a589c1ff2413174a9ac086cb4552
brianhouse <brian.house@gmail.com>
2014-07-14 20:31:58 -0400

    fixing homepage issue

--- sender_persisting.py
+++ sender_persisting.py
@@ -5,19 +5,17 @@ from tweeter import t
 def send(user_id):
     if not (random.random() < (1/288) * 5):
         return
-    try:
-        message = util.get_message('messages/persisting.txt')
-        user = t.users.show(user_id=user_id)
-        message = message.replace("SCREENNAME", user['screen_name'])
-        message = message.replace("FOLLOWERS", str(user['followers_count']))
+    message = util.get_message('messages/persisting.txt')
+    user = t.users.show(user_id=user_id)
+    message = message.replace("SCREENNAME", user['screen_name'])
+    message = message.replace("FOLLOWERS", str(user['followers_count']))
+    if "HOMEPAGE" in message:
         try:
             message = message.replace("HOMEPAGE", user['entities']['url']['urls'][0]['expanded_url'])
-        except IndexError:
+        except IndexError as e:
             return
-        log.info("Sending \"%s\"" % message)
-        t.statuses.update(status=message)
-    except Exception as e:
-        log.error(log.exc(e))
+    log.info("Sending \"%s\"" % message)
+    t.statuses.update(status=message)
 
 if __name__ == "__main__":
     from twitter import Twitter, OAuth
