c1134db5d7f10cf188f3726a1429f7a06f75ad28
tegacodes <tega.brain@gmail.com>
2014-08-12 23:22:33 +1000

    sender_alone fix

--- sender_alone.py
+++ sender_alone.py
@@ -1,8 +1,17 @@
-import random, util
+import json, util, random
+from housepy import config, log
 from tweeter import t
 
+
 def send():
-    if random.random() < (1/288) * 3:
-        message = util.get_message('messages/alone.txt')
-        log.info("Sending \"%s\"" % message)
-        t.statuses.update(status=message)
+    if not(random.random() < (1/288) * 3):
+        return
+    message = util.get_message('messages/alone.txt')
+    log.info("Sending \"%s\"" % message)
+    t.statuses.update(status=message)
+
+if __name__ == "__main__":
+    from twitter import Twitter, OAuth
+    t = Twitter(auth=OAuth(config['twitter']['access_token'], config['twitter']['access_token_secret'], config['twitter']['consumer_key'], config['twitter']['consumer_secret']))
+    user_id = 5452482
+    send(user_id)
